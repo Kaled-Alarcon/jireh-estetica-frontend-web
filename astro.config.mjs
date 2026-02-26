989b3e148fd13b0bf045a234ca48f3968dab1691
Kaled Alarcón <kaledalarcon.cafe@gmail.com>
2026-02-26 18:34:44 -0300

    chore: update astro config and deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   base: "/jireh-estetica-frontend-web/",
+   site: "https://Kaled-Alarcon.github.io/",
+});
